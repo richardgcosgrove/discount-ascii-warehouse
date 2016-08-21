72c2fc5de80ac6b63bcedcdf2b3e474c15259eb5
Richard Cosgrove <richardgcosgrove@gmail.com>
2016-08-21 16:12:54 -0400

    Changed ad filtering to meet specifications
    Old logic would ensure no duplicates, new one makes sure same ad doesn't
    appear twice... however, based on server logic number sent is % 16, so
    technically should check against that *but* assuming that is just for
    simplicity and each r sent is considered unique. For real client I'd get
    clarification from either the client or whoever wrote the server logic.

--- static/scripts/ads.js
+++ static/scripts/ads.js
@@ -1,16 +1,18 @@
-ads = [];
+let lastAdId = null;
+
+function generateAdId(){
+    return Math.floor(Math.random() * 10000);
+}
 
 getAd = function getAd() {
 
-    function hasAd(num) {
-        return window.ads.indexOf(num) != -1;
+    let newAd = generateAdId();
+
+    while (lastAdId === newAd) {
+        newAd = generateAdId();
     }
 
-    let newAd = Math.floor(ads.length * 0.0005) + Math.floor(Math.random() * 10000);
-    while (hasAd(newAd)) {
-    console.log(`Duplicate ad ${newAd}`);
-        newAd = Math.floor(ads.length * 0.0005) + Math.floor(Math.random() * 10000);
-    }
-        window.ads.push(newAd);
+    lastAdId = newAd;
+    
     return newAd;
 }
